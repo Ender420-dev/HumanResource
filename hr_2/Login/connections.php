d08b6aca5de1e37febc87d73d8d364f2efeafd1a
hikarusan19 <emmarjohncanas1@gmail.com>
2025-05-14 20:08:33 +0800

    Update connections.php

--- hr_2/Login/connections.php
+++ hr_2/Login/connections.php
@@ -1,5 +1,5 @@
 <?php 
-    $Connection = mysqli_connect ("localhost:3306","root","","hr2");
+    $Connection = mysqli_connect ("localhost:3307","root","","hr2");
         if(mysqli_connect_errno()){
             echo"failed to connect in mySQL:" .mysqli_connect_error();
         }else{

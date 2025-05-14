8202c6091e96d7c00d0becb6cfbbdb13788f2063
hikarusan19 <emmarjohncanas1@gmail.com>
2025-05-14 20:09:12 +0800

    Update conn.php

--- phpcon/conn.php
+++ phpcon/conn.php
@@ -1,6 +1,6 @@
 
 <?php
-$connection = mysqli_connect("localhost:3307", "root", "", "hr2");
+$connection = mysqli_connect("localhost:3306", "root", "", "hr2");
 
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
